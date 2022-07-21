fca214b1b09d648dc7eb8bdf1c6c19573b5c0b73
Edoardo Costantini <edoardo.costanitni.96@gmail.com>
2022-07-21 20:44:54 +0200

    implemented naive benches

--- benches/priority_queue.rs
+++ benches/priority_queue.rs
@@ -1,5 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use mora_queue::{dumb_priority_queue::DumbPriorityQueue, priority_queue::PriorityQueue};
+use mora_queue::{
+    dumb_priority_queue::DumbPriorityQueue, naive_priority_queue::NaivePriorityQueue,
+    priority_queue::PriorityQueue,
+};
 
 fn dumb_enqueue(c: &mut Criterion) {
     let mut pq = DumbPriorityQueue::<i32, i32>::default();
@@ -28,5 +31,34 @@ fn dumb_dequeue(c: &mut Criterion) {
     c.bench_function("dumb_dequeue", |b| b.iter(|| pq.dequeue(black_box(100))));
 }
 
-criterion_group!(queue_benches, dumb_enqueue, dumb_dequeue);
-criterion_main!(queue_benches);
+fn naive_enqueue(c: &mut Criterion) {
+    let mut pq = NaivePriorityQueue::<i32, i32>::default();
+    let mut count = 0;
+    c.bench_function("naive_enqueue", |b| {
+        b.iter(|| {
+            count = count + 1;
+            pq.enqueue(black_box(count), black_box(-count))
+        })
+    });
+}
+
+fn naive_dequeue(c: &mut Criterion) {
+    let mut pq = NaivePriorityQueue::<i32, i32>::default();
+
+    (0..10000).for_each(|x| match x % 2 {
+        1 => {
+            pq.enqueue(-x, x);
+        }
+        0 => {
+            pq.enqueue(x, x);
+        }
+        _ => {}
+    });
+
+    c.bench_function("naive_dequeue", |b| b.iter(|| pq.dequeue(black_box(100))));
+}
+
+criterion_group!(dumb_benches, dumb_enqueue, dumb_dequeue);
+criterion_group!(naive_benches, naive_enqueue, naive_dequeue);
+
+criterion_main!(dumb_benches, naive_benches);
