d8075eb57e21a008f7fe006b7ea94f168f0052a5
Edoardo Costantini <edoardo.costanitni.96@gmail.com>
2022-07-30 00:31:25 +0200

    fixed get queue and add queue

--- mora-core/src/lib.rs
+++ mora-core/src/lib.rs
@@ -20,10 +20,13 @@ impl MoraContext {
         self.temporal_queues = Default::default()
     }
 
-    pub fn add_queue(&mut self, arg: &str) -> Option<()> {
-        self.temporal_queues
-            .insert(arg.to_owned(), Default::default())
-            .map(|_| ())
+    pub fn add_queue(&mut self, queue_name: String) -> Result<(), String> {
+        if let true = self.temporal_queues.contains_key(&queue_name) {
+            return Err("queue already exists".to_owned());
+        }
+
+        self.temporal_queues.insert(queue_name, Default::default());
+        Ok(())
     }
 
     pub fn get_queue(&mut self, queue_name: &str) -> Option<&mut TemporalQueue<Value>> {
@@ -37,12 +40,34 @@ mod tests {
     #[test]
     fn clear_clears_context() -> Result<(), String> {
         let mut context: MoraContext = Default::default();
-        context.add_queue("test_queue");
+        context.add_queue("test_queue".to_string())?;
         let queue = context.get_queue("test_queue").unwrap();
         queue.enqueue(1, "test".as_bytes().to_owned())?;
         assert_eq!(queue.len, 1);
         context.clear();
         assert!(context.get_queue("test_queue").is_none());
+        Ok(())
+    }
+
+    #[test]
+    fn add_queue_adds_queue() -> Result<(), String> {
+        let mut context: MoraContext = Default::default();
+        context.add_queue("test_queue_1".to_string())?;
+        context.add_queue("test_queue_2".to_string())?;
+        let queue = context.get_queue("test_queue_1");
+        assert!(queue.is_some());
+        let queue = context.get_queue("test_queue_2");
+        assert!(queue.is_some());
+        Ok(())
+    }
+
+    #[test]
+    fn add_queue_doesnt_add_if_queue_already_exists() -> Result<(), String> {
+        let mut context: MoraContext = Default::default();
+        let result = context.add_queue("test_queue_1".to_string());
+        assert!(result.is_ok());
+        let result = context.add_queue("test_queue_1".to_string());
+        assert!(result.is_err());
         Ok(())
     }
 }
