489dcaa38a63bce0dab526eef9b7334000843671
Edoardo Costantini <edoardo.costanitni.96@gmail.com>
2022-07-29 23:43:57 +0200

    impl temporal queue

--- mora-queue/src/priority_queue/mod.rs
+++ mora-queue/src/priority_queue/mod.rs
@@ -6,6 +6,7 @@ pub trait PriorityQueue<K, V> {
     fn len(&self) -> usize;
     fn enqueue(&mut self, key: K, value: V) -> Option<V>;
     fn dequeue(&mut self, count: usize) -> Vec<V>;
+    fn peek(&self) -> Option<(K, V)>;
 }
 
 #[cfg(test)]
@@ -47,6 +48,24 @@ macro_rules! priority_queue_tests {
         assert_eq!(values.len(), 3);
         assert_eq!(values, [1, 2, 3]);
         assert_eq!(pq.len(), 1)
+        }
+
+        #[test]
+        fn peek_returns_first_item(){
+            let mut pq = <$type>::default();
+            pq.enqueue(4, 3);
+            pq.enqueue(2, 2);
+            pq.enqueue(1, 1);
+            pq.enqueue(3, 3);
+            let value= pq.peek();
+            assert_eq!(value, Some((1,1)));
+        }
+
+        #[test]
+        fn peek_on_empty_queue_return_none(){
+            let pq = <$type>::default();
+            let value= pq.peek();
+            assert_eq!(value, None);
         }
     }
     )*
